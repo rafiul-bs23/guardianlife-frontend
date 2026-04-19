faad5f90be8fdb0deb7621ae39ef4db0f8abeec0
Rafiul-BS23 <rafiul.islam@brainstation-23.com>
2026-04-19 19:41:18 +0600

    Fix show password icon

--- src/features/login/components/LoginForm.tsx
+++ src/features/login/components/LoginForm.tsx
@@ -97,8 +97,8 @@ const LoginForm = () => {
               className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-400 hover:text-primary transition-colors"
             >
               {showPassword
-                ? <EyeOff className="w-5 h-5" />
-                : <Eye className="w-5 h-5" />}
+                ? <Eye className="w-5 h-5" />
+                : <EyeOff className="w-5 h-5" />}
             </button>
           </div>
         </div>
